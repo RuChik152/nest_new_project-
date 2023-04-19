eabe7d0303ff5f6ae3f2e2204e4189ce37bdf78f
Nikita Chernov <info@nchernov.ru>
2023-04-19 15:49:00 +0300

    update cervices auth add emailJS

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User, AdminAuthDocument } from './auth.schema';
 import { Model } from 'mongoose';
 import { AdminUserDto } from './auth.dto';
+import emailjs from '@emailjs/nodejs';
+import * as process from "process";
 
 @Injectable()
 export class AuthService {
@@ -28,6 +30,21 @@ export class AuthService {
     })
     if (!user) {
       const {email, name} = await this.authModel.create(userData);
+      const templateParams = {
+        name,
+        mail: email,
+      };
+      emailjs.send(`${process.env.EMAIL_JS_SERVICE_ID}`,`${process.env.EMAIL_JS_TEMPLATE_ID}`,templateParams, {
+        publicKey: process.env.EMAIL_JS_PUBLICK_KEY,
+        privateKey: process.env.EMAIL_JS_PRIVATE_KEY,
+      }).then(
+        (response) => {
+          console.log('SUCCESS!', response.status, response.text);
+        },
+        (err) => {
+          console.log('FAILED...', err);
+        },
+      );
       return { email, name, state: "new" }
     }
     return {user, state: "old"}
