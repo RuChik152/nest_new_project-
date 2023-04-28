9b9659b7d4b92c9251de062d306303d30b5fbf3f
Nikita Chernov <info@nchernov.ru>
2023-04-28 12:55:50 +0300

    fix path

--- src/lib/Compressor.ts
+++ src/lib/Compressor.ts
@@ -35,7 +35,8 @@ export class Compressor {
             this.#pathExe = process.env.PATH_TO_ZIP_LINUX
             break;
         }
-        child_process.execFileSync(this.#pathExe, ['a', '-tzip', '-spe', '-mx5', '-r0', this.#pathStore, `${this.#pathFolder}\\*`])
+        // child_process.execFileSync(this.#pathExe, ['a', '-tzip', '-spe', '-mx5', '-r0', this.#pathStore, `${this.#pathFolder}\\*`])
+        child_process.execFileSync(this.#pathExe, ['a', '-tzip', '-spe', '-mx5', '-r0', this.#pathStore, path.resolve(`${this.#pathFolder}`, '\\*')])
       } catch (error) {
         console.log('ERROR', error)
       }
