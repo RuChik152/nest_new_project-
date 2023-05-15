7dc6b567246e330b9a4d341322e7155aaf9cbd1b
Nikita Chernov <info@nchernov.ru>
2023-05-15 20:50:57 +0300

    modified data history

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   //const app = await NestFactory.create(AppModule, expressAdapter);
 
   app.enableCors({
-    origin: "https://*"
+    origin: "*"
   });
 
   app.useGlobalPipes(
